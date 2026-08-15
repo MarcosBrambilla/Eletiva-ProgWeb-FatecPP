fa81578448fd72792e6d40435129b6192c47db31
MarcosBrambilla <marcos.brambilla2003@gmail.com>
2026-08-15 17:00:33 -0300

    Adicionado a pasta de atividade para o 5ºSemestre, eletivas 2

---
+++ Eletiva2/Exercicios/app/Http/Controllers/ExerciciosController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExerciciosController extends Controller
+{
+    public function abrirFormExer1()
+    {
+        return view('exer1');
+    }
+    public function respostaExer1(Request $request)
+    {
+        $valor1 = $request->valor1;
+        $valor2 = $request->valor2;
+
+        $soma = $valor1 + $valor2;
+
+        return view('exer1', ['soma' => $soma]);
+    }
+
+    public function abrirFormExer2()
+    {
+        return view('exer2');
+    }
+    public function respostaExer2(Request $request)
+    {
+        $valor1 = $request->valor1;
+        $valor2 = $request->valor2;
+
+        $subtracao = $valor1 - $valor2;
+
+        return view('exer2', ['subtracao' => $subtracao]);
+    }
+}
---
+++ Eletiva2/Exercicios/routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExerciciosController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/exer1', [ExerciciosController::class, 'abrirFormExer1']);
+Route::post('/exer1resp', [ExerciciosController::class, 'respostaExer1']);
+
+Route::get('/exer2', [ExerciciosController::class, 'abrirFormExer2']);
+Route::post('/exer2resp', [ExerciciosController::class, 'respostaExer2']);
